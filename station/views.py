bc9f491d87e4f75823586471f9189d1db077ecda
Roksolana Kalinska <woops359@gmail.com>
2025-05-01 20:36:00 +0300

    Edit: BusListView and BusDetailView written with GenericView

--- station/views.py
+++ station/views.py
@@ -1,40 +1,13 @@
-from rest_framework.response import Response
-from rest_framework import status, generics, mixins
-from rest_framework.decorators import api_view
-from rest_framework.generics import get_object_or_404
-from rest_framework.views import APIView
+from rest_framework import generics
 
 from station.models import Bus
 from station.serializers import BusSerializer
 
 
-class BusListView(generics.GenericAPIView,
-                  mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  mixins.UpdateModelMixin):
-
+class BusListView(generics.ListCreateAPIView):
     queryset = Bus.objects.all()
     serializer_class = BusSerializer
 
-    def get(self, request, *args, **kwargs) -> Response:
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs) -> Response:
-        return self.create(request, *args, **kwargs)
-
-class BusDetailView(generics.GenericAPIView,
-                    mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin):
-
+class BusDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Bus.objects.all()
     serializer_class = BusSerializer
-
-    def retrieve(self, request, *args, **kwargs) -> Response:
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs) -> Response:
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs) -> Response:
-        return self.destroy(request, *args, **kwargs)
