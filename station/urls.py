2ca00cd00757cb5715874e94532898c388a84687
Roksolana Kalinska <woops359@gmail.com>
2025-05-01 20:54:46 +0300

    Edited: Views.py is writen with ViewSet

--- station/urls.py
+++ station/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
-from station.views import BusListView, BusDetailView
+from station.views import BusViewSet
 
 app_name ="station"
 
+bus_list = BusViewSet.as_view(actions={"get": "list", "post": "create"})
+bus_detail = BusViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "path": "partial_update",
+        "delete": "destroy"
+    }
+)
+
 urlpatterns = [
-    path("buses/", BusListView.as_view(), name="bus_list"),
-    path("buses/<int:pk>/", BusDetailView.as_view(), name="bus_detail"),
+    path("buses/", bus_list, name="bus_list"),
+    path("buses/<int:pk>/", bus_detail, name="bus_detail"),
 
 ]
