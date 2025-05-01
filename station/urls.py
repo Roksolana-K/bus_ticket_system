8c216f88061ca13d7dcaa8092cf7abf600ec3bc8
Roksolana Kalinska <woops359@gmail.com>
2025-05-01 21:01:56 +0300

    Edited: urls.py -> used routers

--- station/urls.py
+++ station/urls.py
@@ -1,20 +1,10 @@
 from django.urls import path
 from station.views import BusViewSet
+from rest_framework import routers
 
 app_name ="station"
 
-bus_list = BusViewSet.as_view(actions={"get": "list", "post": "create"})
-bus_detail = BusViewSet.as_view(
-    actions={
-        "get": "retrieve",
-        "put": "update",
-        "path": "partial_update",
-        "delete": "destroy"
-    }
-)
+router = routers.DefaultRouter()
+router.register("buses", BusViewSet)
 
-urlpatterns = [
-    path("buses/", bus_list, name="bus_list"),
-    path("buses/<int:pk>/", bus_detail, name="bus_detail"),
-
-]
+urlpatterns = router.urls
